ef87e24a6dbc0f2b4841717a1b5ed90e2a91fb97
Merlino79 <alessandro.ciancio@hotmail.com>
2021-06-24 20:21:56 +0200

    prova sistemazione errore nel passaggio dati tabella non trovata

--- database/seeds/Db_holidaysTableSeed.php
+++ database/seeds/Db_holidaysTableSeed.php
@@ -1,5 +1,5 @@
 <?php
-use App\Holyday;
+use App\Holiday;
 use Illuminate\Database\Seeder;
 
 class Db_holidaysTableSeed extends Seeder
@@ -9,21 +9,34 @@ class Db_holidaysTableSeed extends Seeder
      *
      * @return void
      */
-               //$table->id();
-    //         $table->float('price', 7, 2);
-    //         $table->tinyInteger('rooms')->unsigned();
-    //         $table->string('destination', 30);
-    //         $table->string('type_of_accommodation', 20);
-    //         $table->boolean('car_parking')->default(1);
-    //         $table->string('season', 10);
-    //         $table->timestamps();
+            // $table->string('reference', 12);
+            // $table->string('address', 100);
+            // $table->string('postal_code', 10);
+            // $table->string('city', 50);
+            // $table->string('state', 50);
+            // $table->float('price', 7, 2);
+            // $table->tinyInteger('rooms')->unsigned();
+            // $table->string('destination', 30);
+            // $table->string('type_of_accommodation', 20);
+            // $table->boolean('car_parking')->default(1);
+            // $table->string('season', 10);
+            // $table->timestamps();
+
     public function run()
     {
-        $holyday = new Holyday();
-        $holyday->price ="1500";
-        $holyday->room ="5";
-        $holyday->type_of_accommodation ="apartment";
-        $holyday->season ="winter";
+        $holiday = new Holiday();
+        $holiday->refence = "ale_agency";
+        $holiday->addres = "via dei gelsomini";
+        $holiday->postal_code =" 20100";
+        $holiday->city =" Milano";
+        $holiday->state ="italia";
+        $holiday->price ="1500";
+        $holiday->rooms ="5";
+        $holiday->destination ="portogallo";
+        $holiday->type_of_accommodation ="apartman";
+        $holiday->season ="winter";
+        $holiday->save();
+       
     }
 }
  
